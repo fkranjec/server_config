9c586679279bd73170bc45a50c37d051b3900b27
filip.kranjec <filip.kranjec@invariant.hr>
2025-08-07 10:55:01 +0200

    matrix + element

--- system/configuration.nix
+++ system/configuration.nix
@@ -164,6 +164,21 @@ services.mattermost = {
     enable = true;
     settings = {
       server_name = "matrix.homelab.com.hr";
+      listeners = [
+        {
+          port = 8008;
+          bind_addresses = [ "::1" ];
+          type = "http";
+          tls = false;
+          x_forwarded = true;
+          resources = [
+            {
+              names = [ "client" "federation" ];
+              compress = false;
+            }
+          ];
+        }
+      ];
     };
   };
    
