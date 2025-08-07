1b13c23f39a4b104f4011a0da754c901a8cde969
filip.kranjec <filip.kranjec@invariant.hr>
2025-08-07 11:07:45 +0200

    matrix + element

--- system/configuration.nix
+++ system/configuration.nix
@@ -160,14 +160,23 @@ services.mattermost = {
     
   };
 
+  services.postgresql.initialScript = pkgs.writeText "synapse-init.sql" ''
+    CREATE ROLE "matrix-synapse" WITH LOGIN PASSWORD 'strong-password';
+    CREATE DATABASE "matrix-synapse" WITH OWNER "matrix-synapse"
+      TEMPLATE template0
+      LC_COLLATE = "C"
+      LC_CTYPE = "C";
+  '';
+
   services.matrix-synapse = {
     enable = true;
     settings = {
       server_name = "matrix.homelab.com.hr";
+
       listeners = [
         {
           port = 8008;
-          bind_addresses = [ "::1" ];
+          bind_addresses = [ "127.0.0.1" ];
           type = "http";
           tls = false;
           x_forwarded = true;
