63e14e24ebb01af8d61190d41c6fcfd4fcca8e97
filip.kranjec <filip.kranjec@invariant.hr>
2025-08-07 18:09:10 +0200

    fix: dohvacanje slika pogreska

--- system/nginx.nix
+++ system/nginx.nix
@@ -32,12 +32,6 @@ in
       locations."/" = {
         proxyPass = "${localhost}:${toString port_forgejo}";
         proxyWebsockets = true;
-        extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
-        '';
       };
     };
     virtualHosts."keycloak.${domain}" = {
