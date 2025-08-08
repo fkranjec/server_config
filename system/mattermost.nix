5d616e10bbb970781ee3bf72fb6aca9af867e40a
filip.kranjec <filip.kranjec@invariant.hr>
2025-08-08 20:09:14 +0200

    fix: mattermost

--- system/mattermost.nix
+++ system/mattermost.nix
@@ -1,12 +1,12 @@
 {pkgs, name, domain, port_mattermost,...}:
 let
-  secret = builtins.readFile "/run/secrets/gitlab";
 in
 {
   services.mattermost = {
     enable = true;
     siteName = name;
     siteUrl = "https://chat.${domain}";
+    environmentFile = /etc/nixos/secrets/gitlab;
 
     extraConfig = {
       EmailSettings = {
@@ -16,7 +16,7 @@ in
       };
       GitLabSettings= {
         Enable= true;
-        Secret= secret;
+        Secret= "$MM_GITLAB_SECRET";
         Id= "mattermost";
         Scope= "profile email";
         AuthEndpoint= "https://keycloak.${domain}/cloak/realms/homelab/protocol/openid-connect/auth";
