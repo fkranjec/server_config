a5cda1f5bf1c643b2ff1de9b2593a6e8464413fa
filip.kranjec <filip.kranjec@invariant.hr>
2025-08-08 14:46:31 +0200

    fix: mattermost

--- system/mattermost.nix
+++ system/mattermost.nix
@@ -4,6 +4,6 @@
     enable = true;
     siteName = "Homelab";
     siteUrl = "https://chat.${domain}";
-    configurationFile = "/etc/nixos/secrets/mattermost_config.json";
+    configDir = "/etc/nixos/secrets/mattermost.json";
   };
 }
