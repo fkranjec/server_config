a71dcbca1be5460462d91917bf1d02bd0d47acc6
filip.kranjec <filip.kranjec@invariant.hr>
2025-08-08 15:12:49 +0200

    fix: mattermost

--- system/mattermost.nix
+++ system/mattermost.nix
@@ -4,5 +4,20 @@
     enable = true;
     siteName = "Homelab";
     siteUrl = "https://chat.${domain}";
+
+    extraConfig = {
+      GitLabSettings= {
+        Enable= true;
+        Secret= "pKOPVDTif7qmjSQ9Fv8Cog1jbMEhWS2O";
+        Id= "mattermost";
+        Scope= "";
+        AuthEndpoint= "http://keycloak.homelab.com.hr/cloak/realms/homelab/protocol/openid-connect/auth";
+        TokenEndpoint= "http://keycloak.homelab.com.hr/cloak/realms/homelab/protocol/openid-connect/token";
+        UserAPIEndpoint= "http://keycloak.homelab.com.hr/cloak/realms/homelab/protocol/openid-connect/userinfo";
+        DiscoveryEndpoint= "http://keycloak.homelab.com.hr/cloak/realms/homelab/.well-known/openid-configuration";
+        ButtonText= "Login with ICRC Keycloak";
+        ButtonColor= "#ADD015";
+      };
+    };
   };
 }
