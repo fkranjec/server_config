381eaa1c20d1dd247589603b18b5841c205ce11f
filip.kranjec <filip.kranjec@invariant.hr>
2025-11-10 11:40:31 +0100

    fix: excalidraw

--- system/jitsi.nix
+++ system/jitsi.nix
@@ -4,7 +4,27 @@
     enable = true;
     hostName = "jitsi.homelab.com.hr";
     excalidraw.enable = true;
+    config = {
+      whiteboard = {
+        enabled = true;
+        # Jitsi expects the collab server base URL here
+        collabServerBaseUrl = "https://jitsi.homelab.com.hr";
+      };
+      # If you define toolbarButtons anywhere, ensure 'whiteboard' is in there.
+      # If you DON'T set toolbarButtons at all, leave this out.
+      toolbarButtons = [
+        "microphone"
+        "camera"
+        "desktop"
+        "chat"
+        "whiteboard"
+        "raisehand"
+        "tileview"
+        "hangup"
+      ];
+    };
   };
+   
 
   services.jitsi-videobridge.openFirewall = true;
 }
