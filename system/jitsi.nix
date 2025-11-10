dfbd8d9ebc898cee200318c63e00f1bf5c9cedaf
filip.kranjec <filip.kranjec@invariant.hr>
2025-11-10 11:45:45 +0100

    fix: jitsi-excalidraw

--- system/jitsi.nix
+++ system/jitsi.nix
@@ -5,11 +5,6 @@
     hostName = "jitsi.homelab.com.hr";
     excalidraw.enable = true;
     config = {
-      whiteboard = {
-        enabled = true;
-        # Jitsi expects the collab server base URL here
-        collabServerBaseUrl = "https://jitsi.homelab.com.hr";
-      };
       # If you define toolbarButtons anywhere, ensure 'whiteboard' is in there.
       # If you DON'T set toolbarButtons at all, leave this out.
       toolbarButtons = [
