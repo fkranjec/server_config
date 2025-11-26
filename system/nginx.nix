d6e250f1957577fc7c1bef1187183e964ae4427f
filip.kranjec <filip.kranjec@invariant.hr>
2025-11-26 10:18:31 +0100

    gzip settings

--- system/nginx.nix
+++ system/nginx.nix
@@ -9,7 +9,6 @@ in
   };
   services.nginx = {
     enable = true;
-    recommendedGzipSettings = true;
     recommendedOptimisation = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
